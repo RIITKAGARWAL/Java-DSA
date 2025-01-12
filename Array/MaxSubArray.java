3f505f61fa942215d954f564cc0a3d08b01e4bbd
Ritik Agarwal <110211755+RIITKAGARWAL@users.noreply.github.com>
2025-01-12 18:25:00 +0530

    added negative case in maxSubArray

--- Array/MaxSubArray.java
+++ Array/MaxSubArray.java
@@ -1,9 +1,29 @@
 public class MaxSubArray{
     static int arr[] = {5,4,-1,7,8};
+
+    //check if all elements in the array is negative
+    static boolean isNegative(int arr[]){
+        for(int num:arr){
+            if(num>0){
+                return false;
+            }
+        }
+        return true;
+    }
     //brute force algorithm
     static int maxSum(int arr[]){
-        int max= 0;
+        int max= Integer.MIN_VALUE;
         int currSum = 0;
+
+                //check if all elements in the array is negative
+                if(isNegative(arr)){
+                    for(int num: arr){
+                        if(num>max){
+                            max = num;
+                        }
+                    }
+                    return max;
+                }
 
         for(int i=0;i<arr.length;i++){
             currSum=0;
@@ -19,9 +39,21 @@ public class MaxSubArray{
 
     //via kaden's algorithm
     static int maxSumKaden(int arr[]){
-        int max= 0;
+        double max= Double.NEGATIVE_INFINITY;
         int currSum = 0;
         
+        //check if all elements in the array is negative
+        if(isNegative(arr)){
+            for(int num: arr){
+                if(num>max){
+                    max = num;
+                }
+            }
+            return (int)max;
+        }
+
+
+
         for(int ctr=0;ctr<arr.length;ctr++){
             if(currSum<0){
                 currSum=0;
@@ -31,7 +63,7 @@ public class MaxSubArray{
                 max = currSum;
             }
         }
-        return max;
+        return (int)max;
     }
     public static void main(String args[]){
         System.out.println(maxSum(arr));
