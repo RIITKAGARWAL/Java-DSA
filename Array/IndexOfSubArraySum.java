7c791888f2f9fe586570d3908f8addecf4b9cc18
Ritik Agarwal <110211755+RIITKAGARWAL@users.noreply.github.com>
2025-01-16 13:10:33 +0530

    Index of SubAray Sum Question solved

---
+++ Array/IndexOfSubArraySum.java
@@ -0,0 +1,30 @@
+public class IndexOfSubArraySum {
+    
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        ArrayList<Integer> tempArr = new ArrayList<>();
+        int sum = 0;
+        int firstPtr = 0;
+        int lastPtr = 0;
+
+        while (lastPtr <= arr.length) {
+            if (sum == target) {
+                tempArr.add(firstPtr + 1);
+                tempArr.add(lastPtr);
+                return tempArr;
+            }
+            if (sum > target && firstPtr < lastPtr) {
+                sum -= arr[firstPtr];
+                firstPtr++;
+            } else if (lastPtr < arr.length) {
+                sum += arr[lastPtr];
+                lastPtr++;
+            } else {
+                break;
+            }
+        }
+
+        tempArr.add(-1);
+        return tempArr;
+    }
+}
+
